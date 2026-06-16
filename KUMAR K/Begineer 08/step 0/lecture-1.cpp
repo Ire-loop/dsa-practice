d5e7dc423dce4c304f6077b80cc42a3841451bd0
Ire-loop <bhaveshdeshmukh109@outlook.com>
2026-06-16 21:26:47 +0530

    lecture-1: easy

---
+++ KUMAR K/Begineer 08/step 0/lecture-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// https://docs.google.com/document/d/1RUssam21RADmNgVxweYxk4BLn8MJCxPxxQIvaO1InAo/edit?addon_store&tab=t.0
+int main() {
+
+    /*  PART 1
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 6; j++) {
+                cout << "0";
+            }
+
+            if (i != 3) cout << endl;
+        }
+    */
+    
+    /*  PART 2
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                cout << "0";
+            }
+
+            cout << endl;
+            
+        }
+    */
+    
+    return 0;
+}
